8e98be258009c88669961f25598ebd2a18d87d5d
fdesimini <fdesimini@users.noreply.github.com>
2015-08-25 13:57:35 -0400

    First commit plus:
    Step 1 - haven't implemented the layerView class yet

---
+++ Kindling/CardViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CardViewController.swift
+//  Kindling
+//
+//  Created by Frank Desimini on 2015-08-25.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        roundViewCorners()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
